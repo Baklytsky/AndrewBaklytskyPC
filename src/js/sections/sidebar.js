dd9483d69ecaa3db88c47e250e8774613b3a5d7e
Ivan Ivanov <reinosko@gmail.com>
2024-11-26 17:58:25 +0200

    remove map, countdown, subcollections, supporting-menu, announcement, logo-list, sidebar, popups

--- src/js/sections/sidebar.js
+++
@@ -1,5 +0,0 @@
-import {register} from '../vendor/theme-scripts/theme-sections';
-import scrollToElement from '../features/scroll-to-element';
-import scrollSpy from '../features/scroll-spy';
-
-register('sidebar', [scrollToElement, scrollSpy]);
