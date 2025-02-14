56b26cc4a66188800c4fb995b200d43c7a482b54
Andrew Baklytsky <baklytsky@gmail.com>
2025-02-14 10:38:59 +0200

    Add server.watcher for additional entrypoints

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import cleanup from '@by-association-only/vite-plugin-shopify-clean'
 import pageReload from 'vite-plugin-page-reload'
 import tailwindcss from '@tailwindcss/vite'
 import autoprefixer from 'autoprefixer'
+import path from 'path'
+import fs from 'fs'
 
 export default defineConfig({
   plugins: [
@@ -17,8 +19,9 @@ export default defineConfig({
       entrypointsDir: 'src/entrypoints',
       // Additional files to use as entry points (accepts an array of file paths or glob patterns)
       additionalEntrypoints: [
-          'src/scss/sections/*.scss',
-          'src/js/sections/*.scss',
+        'src/scss/inline/*.scss',
+        'src/scss/sections/*.scss',
+        'src/js/sections/*.js',
       ],
       // Specifies the file name of the snippet that loads your assets
       snippetFile: 'vite-tag.liquid',
@@ -29,18 +32,53 @@ export default defineConfig({
     }),
     pageReload('/tmp/theme.update', {
       delay: 2000
-    })
+    }),
+    {
+      name: 'vite-plugin-css-to-liquid',
+      generateBundle(options, bundle) {
+        Object.keys(bundle).forEach(fileName => {
+          if (fileName.endsWith('.css') && fileName.startsWith('inline-')) {
+            const source = bundle[fileName].source;
+            const liquidContent = `{% style %}\n${source}\n{% endstyle %}`;
+            const fileNameWithoutHash = path.basename(fileName, '.min.css').split('.')[0];
+            const liquidFileName = fileNameWithoutHash + '.liquid';
+            const outputDir = path.resolve(__dirname, 'snippets');
+            if (!fs.existsSync(outputDir)) {
+              fs.mkdirSync(outputDir, { recursive: true });
+            }
+            fs.writeFileSync(path.join(outputDir, liquidFileName), liquidContent);
+          }
+        });
+      },
+    },
+    {
+      name: 'watch-additional-entrypoints',
+      apply: 'serve',
+      configureServer(server) {
+        server.watcher.add([
+          'src/scss/inline/*.scss',
+          'src/scss/sections/*.scss',
+          'src/js/sections/*.js',
+        ]);
+
+        server.watcher.on('change', (filePath) => {
+          if (filePath.endsWith('.scss') || filePath.endsWith('.js')) {
+            server.ws.send({ type: 'full-reload' });
+          }
+        });
+      }
+    }
   ],
   css: {
     preprocessorOptions: {
       scss: {
         api: 'modern-compiler'
       },
-      plugins: [
-        tailwindcss(),
-        autoprefixer(),
-      ]
     },
+    plugins: [
+      tailwindcss(),
+      autoprefixer(),
+    ],
   },
   build: {
     sourcemap: true,
