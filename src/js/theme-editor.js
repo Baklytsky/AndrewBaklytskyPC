5ee4ddbc217005e89cb5881af088c2bb1139b046
Ivan Ivanov <reinosko@gmail.com>
2025-02-14 13:45:40 +0200

    clean up settings and update newsletter-form file to accept section settings as well

--- src/js/theme-editor.js
+++ src/js/theme-editor.js
@@ -1,6 +1,6 @@
 document.addEventListener('shopify:section:select', (event) => {
   // Popup components
-  const popupComponent = event.target.classList.contains('shopify-section-popups') ? event.target.querySelector('popup-component') : null;
+  const popupComponent = event.target.classList.contains('shopify-section-popups') ? event.target.querySelector('popup-component') || event.target.querySelector('popup-newsletter') : null;
   if (popupComponent) {
     popupComponent.classList.add('popup--selected');
 
@@ -16,7 +16,7 @@ document.addEventListener('shopify:section:select', (event) => {
 
 document.addEventListener('shopify:section:deselect', (event) => {
   // Popup components
-  const popupComponent = event.target.classList.contains('shopify-section-popups') ? event.target.querySelector('popup-component') : null;
+  const popupComponent = event.target.classList.contains('shopify-section-popups') ? event.target.querySelector('popup-component') || event.target.querySelector('popup-newsletter') : null;
   if (popupComponent) {
     restorePopups();
   }
@@ -166,23 +166,7 @@ document.addEventListener('shopify:block:deselect', (event) => {
 });
 
 function hideOtherPopups(selectedPopup) {
-  document.querySelectorAll('popup-component')?.forEach((popup) => {
-    if (popup !== selectedPopup) {
-      const dialog = popup.querySelector('dialog');
-      if (dialog.hasAttribute('open')) {
-        popup.classList.add('popup--hidden');
-
-        // Check if browser supports Dialog tags
-        if (typeof dialog.close === 'function') {
-          dialog.close();
-        } else {
-          dialog.removeAttribute('open');
-          dialog.setAttribute('aria-hidden', true);
-        }
-      }
-    }
-  });
-  document.querySelectorAll('popup-newsletter')?.forEach((popup) => {
+  document.querySelectorAll('popup-component, popup-newsletter')?.forEach((popup) => {
     if (popup !== selectedPopup) {
       const dialog = popup.querySelector('dialog');
       if (dialog.hasAttribute('open')) {
