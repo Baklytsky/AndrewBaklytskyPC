6729650af0856aa364d2fcb54f8b6e588694eca3
ivaylo-kolev <ivo@presidiocreative.com>
2025-05-05 14:25:37 +0300

    Update mobile-menu callbacks

--- src/js/theme.js
+++ src/js/theme.js
@@ -27,7 +27,6 @@ import './globals/header-mobile-drawer';
 import './globals/header-mobile-sliderule';
 import './globals/header-search-popdown';
 import './globals/main-search';
-import './globals/mobile-menu';
 import './globals/native-scrollbar';
 import './globals/popout';
 import './globals/popup';
