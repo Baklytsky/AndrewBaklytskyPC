5c6d5c4cb25a96e7fdd2b10babdc5157d3c91224
Ivan Ivanov <reinosko@gmail.com>
2025-07-01 10:33:58 +0300

    add swiper js

--- rollup.config.js
+++ rollup.config.js
@@ -46,6 +46,7 @@ const developmentComment = `
 const globalPackages = {
   '@shopify/theme-rte': 'themeVendor.themeRte',
   'scroll-lock': 'themeVendor.ScrollLock',
+  swiper: 'themeVendor.swiper',
 };
 
 const externalPackages = ['@shopify/theme-product', '@shopify/theme-product-form', '@shopify/theme-rte', 'swiper', 'scroll-lock'];
