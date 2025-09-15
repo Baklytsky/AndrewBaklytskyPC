c21532f6b9681da4456ccf5d235a57f245e90b35
Ivan Ivanov <reinosko@gmail.com>
2025-09-15 15:31:12 +0300

    remove variant image

--- src/js/globals/product-form.js
+++ src/js/globals/product-form.js
@@ -35,8 +35,6 @@ const selectors = {
   section: '[data-section-type]',
   variantSku: '[data-variant-sku]',
   variantFinalSaleMeta: '[data-variant-final-sale-metafield]',
-  variantButtons: '[data-variant-buttons]',
-  variantOptionImage: '[data-variant-option-image]',
   quickAddModal: '[data-quick-add-modal]',
   priceOffAmount: '[data-price-off-amount]',
   priceOffBadge: '[data-price-off-badge]',
@@ -141,12 +139,6 @@ class ProductForm extends HTMLElement {
       this.sellout = new SelloutVariants(this.container, this.productJSON);
     } else {
       console.error('Missing product JSON');
-    }
-
-    this.variantOptionImages = this.container.querySelectorAll(selectors.variantOptionImage);
-    this.variantButtons = this.container.querySelectorAll(selectors.variantButtons);
-    if (this.variantOptionImages.length > 1) {
-      this.optionImagesWidth();
     }
   }
 
@@ -373,25 +365,6 @@ class ProductForm extends HTMLElement {
     } else if (!variant && this.remainingWrapper) {
       this.remainingWrapper.classList.add(classes.remainingUnavailable);
     }
-  }
-
-  optionImagesWidth() {
-    if (!this.variantButtons) return;
-
-    let maxItemWidth = 0;
-
-    requestAnimationFrame(() => {
-      this.variantOptionImages.forEach((item) => {
-        const itemWidth = item.clientWidth;
-        if (itemWidth > maxItemWidth) {
-          maxItemWidth = itemWidth;
-        }
-      });
-
-      this.variantButtons.forEach((item) => {
-        item.style?.setProperty('--option-image-width', maxItemWidth + 'px');
-      });
-    });
   }
 
   getBaseUnit(variant) {
