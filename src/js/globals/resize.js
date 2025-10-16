28667f51b8af6cc52d2576aad7a89d2a64d6228f
Ivan Ivanov <reinosko@gmail.com>
2025-10-16 11:56:03 +0300

    resize fixes

--- src/js/globals/resize.js
+++ src/js/globals/resize.js
@@ -1,6 +1,5 @@
 let lastWindowWidth = window.theme.getWindowWidth();
 let lastWindowHeight = window.theme.getWindowHeight();
-
 let lastScale = window.visualViewport ? window.visualViewport.scale : 1;
 
 function dispatch() {
@@ -25,7 +24,6 @@ function resizeListener() {
     'resize',
     window.theme.debounce(function () {
       // Check if viewport is zoomed (scaled)
-      console.log(window.visualViewport);
       const currentScale = window.visualViewport ? window.visualViewport.scale : 1;
       const zooming = Math.abs(currentScale - lastScale) > 0.05;
 
