2068608d667510ecb859485bb8d537392024e5fd
Ivan Ivanov <reinosko@gmail.com>
2025-10-31 14:59:58 +0200

    update cart images

--- src/js/globals/product-form.js
+++ src/js/globals/product-form.js
@@ -21,7 +21,7 @@ if (!customElements.get('product-form')) {
         const isBundle = this.submitButton.hasAttribute('data-bundle-modal-button');
 
         if (isBundle) {
-          const productJSONhtml = this.container.querySelector('[data-bundle-json]')?.innerHTML;
+          const productJSONhtml = this.querySelector('[data-bundle-json]')?.innerHTML;
           const productJSON = JSON.parse(productJSONhtml);
           const bundleButton = document.querySelector(`[data-bundle-product-button="${productJSON.id}"]`);
 
