a933a4dd65ea238e9bbdca8fa04daedf816a7eac
ivaylo-kolev <ivo@presidiocreative.com>
2025-11-04 16:54:13 +0200

    Fix blinking when hovering over swatches with slider

--- src/js/components/swatch.js
+++ src/js/components/swatch.js
@@ -41,12 +41,18 @@ if (!customElements.get('grid-swatch')) {
           if (this.swatchesStyle == 'text' || this.swatchesStyle == 'text-slider') {
             if (this.swatchesStyle == 'text') return;
 
-            this.swatchCount.addEventListener('mouseenter', () => {
+            const showSwatches = () => {
               if (this.hideSwatchesTimer) clearTimeout(this.hideSwatchesTimer);
 
               this.productInfo.classList.add('no-events');
               this.swatchFieldset.classList.add('is-visible');
-            });
+            };
+
+            this.swatchCount.addEventListener('mouseenter', showSwatches);
+
+            if (this.swatchFieldset) {
+              this.swatchFieldset.addEventListener('mouseenter', showSwatches);
+            }
 
             // Prevent color swatches blinking on mouse move
             this.productInfo.addEventListener('mouseleave', () => {
