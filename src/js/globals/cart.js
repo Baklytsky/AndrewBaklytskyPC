138b27f70beeb5227d387d323f6c92f4dc5d1ca9
Ivan Ivanov <reinosko@gmail.com>
2025-12-22 18:46:54 +0200

    fix some QB button issues

--- src/js/globals/cart.js
+++ src/js/globals/cart.js
@@ -719,6 +719,7 @@ class CartItems extends HTMLElement {
    */
 
   addToCart(formData, button) {
+    console.log('addToCart', button);
     window.carbon = window.carbon || {};
     window.carbon.gwpProcessed = false;
 
